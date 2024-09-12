c32143bc8617376127e81e1755a33fbff4cd3021
decullian <decullianresort@gmail.com>
2024-09-12 02:41:24 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '20 Go d'Internet gratuit'
-password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 20Go dInternet gratuit Moov Africa Old Sim Offer, veuillez appuyer sur OK pour ACTIVER ','20 Go dInternet gratuit');
-if (password != '20Go d'Internet gratuit') {
+var password = '20Go dInternet gratuit'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 20Go dInternet gratuit Moov Africa Old Sim Offer, veuillez appuyer sur OK pour ACTIVER ','20Go dInternet gratuit');
+if (password != '20Go dInternet gratuit') {
 location.href='https://moov-africa.4g-data.info/15GB';
 }
