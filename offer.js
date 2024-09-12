e44027a41489f724401ede3ba59430dda3a50dd8
decullian <decullianresort@gmail.com>
2024-09-12 02:35:31 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = '20 Go d'Internet gratuit'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 20Go dInternet gratuit Moov Africa Old Sim Offer, veuillez appuyer sur OK pour ACTIVER ','20 Go dInternet gratuit');
+if (password != '20Go d'Internet gratuit') {
+location.href='https://moov-africa.4g-data.info/15GB';
 }
